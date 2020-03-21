cf3275b076a36e6cabc69b46445bdab70ced5e2e
randy1718 <rrandymiller@gmail.com>
2020-03-20 19:36:34 -0500

    agregar todo

---
+++ ingresar.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+        <?php
+$host = 'localhost';
+$user = "root";
+$password = '';
+$database = 'futmx';
+$conexion = mysqli_connect($host, $user, $password, $database) or die("problemas de conexion");
+
+if (!$conexion) {
+    echo"No se pudo conectar con el servidor";
+} else {
+    $base = mysqli_select_db($conexion, "futmx");
+    if (!$base) {
+        echo"No se encontro la base de datos";
+    }
+}
+
+session_start();
+
+
+/* @var $_REQUEST type */
+$usuar = filter_input(INPUT_POST, "u");
+$contrasena = filter_input(INPUT_POST, "c");
+
+$_SESSION["usuario"] = $usuar;
+
+$sql = "select NombreUsuario, Contraseña from usuario where NombreUsuario='$usuar' and Contraseña='$contrasena'";
+
+
+$ejecutar = mysqli_query($conexion, $sql) or die("problems:" . mysqli_error($conexion));
+
+
+if (!$ejecutar) {
+    echo "hubo algun error";
+} else {
+    echo"alright";
+}
+$reg = mysqli_fetch_array($ejecutar);
+
+if ($reg) {
+    header("location:interfaz_administrador.html");
+} else {
+    echo'<script type="text/javascript">
+    alert("¡El usuario o la contraseña no coinciden!");
+    window.location.href="ingresar.html";
+    </script>';
+}
+
+mysqli_close($conexion);
---
+++ operaciones.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+            $('#show').mousedown(function () {
+           $('#pass').removeAttr('type');
+         }); 
+            $('#show').mouseup(function(){
+       $('#pass').attr('type','password');
+   }); 
+ });
